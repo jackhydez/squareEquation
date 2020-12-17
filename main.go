5ef22cadb970de7a644225fb686289417775c1b8
jackhydez <Pash.ok92@yandex.ru>
2020-12-17 19:35:57 +0300

    add main loop

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func inputABC() (a, b, c float64) {
 	fmt.Scanf("%f\n", &b)
 	fmt.Printf("input C: ")
 	fmt.Scanf("%f\n", &c)
+	fmt.Println()
 	return
 }
 
@@ -26,18 +27,32 @@ func roots(a, b, d float64) (x1, x2 float64) {
 }
 
 func main() {
-	a, b, c := inputABC()
-	d := discriminant(a, b, c)
 
-	if d < 0 {
-		fmt.Println("non roots")
-	} else if d == 0 {
-		x, _ := roots(a, b, d)
-		fmt.Printf("1 root: %f\n", x)
-	} else {
-		x1, x2 := roots(a, b, c)
-		fmt.Println("have 2 roots:")
-		fmt.Printf("root number 1: %f\n", x1)
-		fmt.Printf("root number 2: %f\n", x2)
+	var isRepeatLoop bool = true
+
+	for isRepeatLoop {
+		a, b, c := inputABC()
+		d := discriminant(a, b, c)
+
+		if d < 0 {
+			fmt.Printf("non roots\n\n")
+		} else if d == 0 {
+			x, _ := roots(a, b, d)
+			fmt.Printf("1 root: %f\n\n", x)
+		} else {
+			x1, x2 := roots(a, b, c)
+			fmt.Println("have 2 roots:")
+			fmt.Printf("root number 1: %f\n", x1)
+			fmt.Printf("root number 2: %f\n\n", x2)
+		}
+
+		// ask user for repeat main program
+		var repeatLoop string = "1"
+		fmt.Print("input 1 for exit\nor any for repeat agian: ")
+		fmt.Scanf("%s\n", &repeatLoop)
+		fmt.Println()
+		if repeatLoop == "1" {
+			isRepeatLoop = false
+		}
 	}
 }
